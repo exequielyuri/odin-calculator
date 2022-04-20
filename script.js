68738c58350608d21a606192674eb74c6ae765c8
Exequiel Garcia <leiuqxre@gmail.com>
2022-04-20 18:53:32 +0800

    Add function that determines what operation to be used

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    if (operator == '+') {
+        return add(a, b);
+    } else if (operator == '-') {
+        return subtract(a, b);
+    } else if (operator == '*') {
+        return multiply(a, b);
+    } else if (operator == '/') {
+        return divide(a, b);
+    } else {
+        console.log(`${operator} is an invalid operator.`);
+    }
+}

8094e027536f2b674d3ea6946b9dcaa73daf075d
Exequiel Garcia <leiuqxre@gmail.com>
2022-04-21 16:52:09 +0800

    Create simple calculator logic

--- script.js
+++ script.js
@@ -28,18 +28,78 @@ function operate(a, b, operator) {
     }
 }
 
-function addToDisplay(button) {
-    const text = button.target.textContent;
-    displayContent += text;
-    updateDisplay();
+function isNumber(str) {
+    return !isNaN(Number(str));
+}
+
+function isFull() {
+    return operands.length >= 2;
+}
+
+function resetAll() {
+    displayContent = "";
+    operands = [];
 }
 
 function updateDisplay() {
     display.textContent = displayContent;
 }
 
+function getResult() {
+    result = operate(Number(operands[0]), Number(operands[1]), operator);
+    operands = [];
+
+    if (result % 1 !== 0) { // if has decimal
+        result = Math.floor(result * 100) / 100; // round to two decimal places
+    }
+    displayContent = `= ${result}`;
+}
+
+function execute(button) {
+    const task = button.target.textContent
+
+    switch (task) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            if (isNumber(displayContent)) {
+                operands.push(displayContent);
+            }
+            
+            if (isFull()) {
+                getResult();
+                operands.push(result)
+            } else {
+                displayContent = "";
+            }
+
+            operator = task;
+            break;
+        case '=':
+            if (isNumber(displayContent)) {
+                operands.push(displayContent);
+            }
+            getResult();
+            break;
+        case 'clear':
+            resetAll();
+            break;
+        default: // must be a number
+            if (!isNumber(displayContent)) {
+                displayContent = "";
+            }
+            displayContent += task;
+    }
+    
+    updateDisplay();
+}
+
 let displayContent = "";
+let operands = [];
+let operator;
+let result;
 
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("#buttons button");
-buttons.forEach((button) => button.addEventListener("click", addToDisplay));
+buttons.forEach((button) => button.addEventListener("click", execute));
